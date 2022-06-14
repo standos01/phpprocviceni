a4d2a2ebb87fb95969608430f1a93eabd40fb070
Michal Bogdan <michal.bogdan@beanstudent.cz>
2022-06-14 09:54:45 +0200

    Zamyšlený Timur a kapr

---
+++ standos01.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='db_auta.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+<img src="IMG_1074.jpg">
+<img src="IMG_1546.jpg">
+</body>
+
+</html>
