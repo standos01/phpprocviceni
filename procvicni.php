5ebda920ea28820a834fca5cfbf4b0aa78cf45cd
Stanislav Tiron <stanislav.tiron@beanstudent.cz>
2022-06-14 08:46:33 +0200

    first commit

---
+++ procvicni.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>lol</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+
+<body>
+    <table>
+        <?php
+
+        for ($i = 1; $i <= 50; $i++) {
+            echo "<tr>";
+
+            for ($e = 0; $e <= 6; $e++) {
+                echo "<td>";
+                echo "Ahoj, svete!";
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+
+        ?>
+    </table>
+</body>
+
+</html>
